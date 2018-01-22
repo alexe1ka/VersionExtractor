2197e79b35fe6396b1908076f423c623c796041c
alexe1ka <leshka_09@bk.ru>
2018-01-22 18:38:52 +0300

    parse description

--- VersionExtractor/catalog_parser.py
+++ VersionExtractor/catalog_parser.py
@@ -20,15 +20,14 @@ def generate_report(hdl_file_list):
 
 def file_parser(filepath):
     file_list = []
-    header_data = []
-    matches_description = re.compile('^.*Description.*\:$')
-    matches_version = re.compile('^.*Version.*\:$')
-    matches_designer = re.compile('^.*Designer.*\:$')
+    header_data = {}
+
     with open(filepath, "r") as file:  # открываем файл
         print("filename: " + file.name)
         for line in file:
             file_list.append(line)
-            header_data = matches_description.findall(line)
+            if "Description" in line:
+                header_data["description"] = line.split(":")[1]
 
     print(header_data)
     return file_list
@@ -37,5 +36,5 @@ def file_parser(filepath):
 if __name__ == "__main__":
     test_dir_path = '//MRS//Project_Quartet//КВАРТЕТ//Разработка//Блок 308-051-01 (СЦВМ)//ПО'
     test_file_path = 'E:/addr_cntr.v'
-    print(file_parser(test_file_path))
+    file_parser(test_file_path)
     # print(find("*.v", test_dir_path))
