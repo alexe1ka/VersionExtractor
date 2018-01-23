2bf12ff072b1d0d75041e38971cbff3602a86457
alexe1ka <leshka_09@bk.ru>
2018-01-23 18:15:42 +0300

    new file parser with universal decoder

--- VersionExtractor/catalog_parser.py
+++ VersionExtractor/catalog_parser.py
@@ -2,6 +2,7 @@ import os
 import fnmatch
 import chardet
 import sys
+from chardet import UniversalDetector
 
 
 def find(pattern, path):
@@ -27,12 +28,12 @@ def file_parser(filepath):
     list_of_file_data = []  # построчный лист с содержимым файла
     header_data = {"modules": []}
 
-    # TODO решить проблему с кодировкой MACINTOSH https://docs.python.org/3/library/codecs.html
-    with open(filepath, "r", encoding="maccyrillic") as file:  # открываем файл maccyrillic
+    with open(filepath, "rb") as file:  # открываем файл maccyrillic
         print("filename: " + file.name)
         for line in file:
-            # line_as_bytes = str.encode(line)
-            # print(line_as_bytes)
+            # print(line)
+            line = universal_decoder(line)
+
             list_of_file_data.append(line)
 
             if "Description" in line:
@@ -47,9 +48,21 @@ def file_parser(filepath):
             if "module " in line and "endmodule" not in line and line.lstrip(" ").startswith("m"):
                 # header_data["modules"] = line.split(" ")[1].split("(")[0]
                 header_data["modules"].append(line.split(" ")[1].split("(")[0])
-
     print(header_data)
     return header_data
+
+
+def universal_decoder(line):
+    # print(chardet.detect(line)['encoding'])
+    if chardet.detect(line)['encoding'] == 'ascii':
+        return line.decode("ascii")
+    if chardet.detect(line)['encoding'] == 'windows-1251':
+        return line.decode("windows-1251", "replace")
+    if chardet.detect(line)['encoding'] == 'MacCyrillic':
+        return line.decode("MacCyrillic")
+    if chardet.detect(line)['encoding'] == 'utf-8':
+        return line.decode("utf-8")
+    return ""
 
 
 # функция для очистки строки от \n  и лишних пробелов
@@ -59,43 +72,15 @@ def clean_string(dirty_string):
     return clean
 
 
-def file_parser_experimental(filepath):
-    list_of_file_data = []  # построчный лист с содержимым файла
-    header_data = {"modules": []}
-
-    # TODO решить проблему с кодировкой MACINTOSH https://docs.python.org/3/library/codecs.html
-    with open(filepath, "r", encoding="maccyrillic") as file:  # открываем файл maccyrillic
-        print("filename: " + file.name)
-        for line in file:
-            # line_as_bytes = str.encode(line)
-            # print(line_as_bytes)
-            list_of_file_data.append(line)
-
-            if "Description" in line:
-                header_data["description"] = clean_string(line.split(":")[1])
-
-            if "Version:" in line:
-                header_data["version"] = clean_string(line.split(":")[1])
-
-            if "Designer:" in line:
-                header_data["designer"] = clean_string(line.split(":")[1])
-
-            if "module " in line and "endmodule" not in line and line.lstrip(" ").startswith("m"):
-                # header_data["modules"] = line.split(" ")[1].split("(")[0]
-                header_data["modules"].append(line.split(" ")[1].split("(")[0])
-
-    print(header_data)
-    return header_data
-
-
 if __name__ == "__main__":
     # test_dir_path = 'E:\!TEST_FOLDER\\v_new'
-    test_dir_path = 'E:\!test'
+    test_dir_path = 'E:\!TEST_FOLDER'
+    # test_dir_path = 'E:\!test'
     # test_dir_path = 'D:\MyFiles\Projects\PyCharmProjects\VersionExtractor\VersionExtractor\!TEST_FOLDER\\v_new'
     # test_file_path = 'E:/addr_cntr.v'
     # test_file_path = 'D:/addr_cntr.v'
     # test_file_path = 'D:/many_modules.v'
     file_list = find("*.v", test_dir_path)
     for f in file_list:
-        file_parser_experimental(f)
+        file_parser(f)
         print('\n')
