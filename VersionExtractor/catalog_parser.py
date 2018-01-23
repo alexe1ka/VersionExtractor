3cb498e7c0f8b5e6595ae6800be50508c35b45a3
alexe1ka <leshka_09@bk.ru>
2018-01-23 16:43:23 +0300

    add correct parser for module

--- VersionExtractor/catalog_parser.py
+++ VersionExtractor/catalog_parser.py
@@ -1,7 +1,6 @@
 import os
 import fnmatch
-import re
-import codecs
+import chardet
 
 
 def find(pattern, path):
@@ -26,11 +25,16 @@ def generate_report(hdl_file_list):
 def file_parser(filepath):
     list_of_file_data = []  # построчный лист с содержимым файла
     header_data = {"modules": []}
+
     # TODO решить проблему с кодировкой MACINTOSH https://docs.python.org/3/library/codecs.html
-    with open(filepath, "r", encoding="maccyrillic") as file:  # открываем файл
+    with open(filepath, "r", encoding="maccyrillic") as file:  # открываем файл maccyrillic
         print("filename: " + file.name)
+
         for line in file:
+            # line_as_bytes = str.encode(line)
+            # print(line_as_bytes)
             list_of_file_data.append(line)
+
             if "Description" in line:
                 header_data["description"] = clean_string(line.split(":")[1])
 
@@ -40,11 +44,12 @@ def file_parser(filepath):
             if "Designer:" in line:
                 header_data["designer"] = clean_string(line.split(":")[1])
 
-            if "module" in line and "endmodule" not in line:
+            if "module " in line and "endmodule" not in line and line.lstrip(" ").startswith("m"):
                 # header_data["modules"] = line.split(" ")[1].split("(")[0]
                 header_data["modules"].append(line.split(" ")[1].split("(")[0])
 
     print(header_data)
+    print("\n")
     return header_data
 
 
@@ -56,10 +61,11 @@ def clean_string(dirty_string):
 
 
 if __name__ == "__main__":
-    # test_dir_path = '//MRS//Project_Quartet//КВАРТЕТ//Разработка//Блок 308-051-01 (СЦВМ)//ПО'
-    test_dir_path = 'D:\MyFiles\Projects\PyCharmProjects\VersionExtractor\VersionExtractor\!TEST_FOLDER\\v_new'
+    # test_dir_path = 'E:\!TEST_FOLDER\\v_new'
+    test_dir_path = 'E:\!test'
+    # test_dir_path = 'D:\MyFiles\Projects\PyCharmProjects\VersionExtractor\VersionExtractor\!TEST_FOLDER\\v_new'
     # test_file_path = 'E:/addr_cntr.v'
-    test_file_path = 'D:/addr_cntr.v'
+    # test_file_path = 'D:/addr_cntr.v'
     # test_file_path = 'D:/many_modules.v'
     file_list = find("*.v", test_dir_path)
     for f in file_list:
