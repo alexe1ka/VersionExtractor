03487efb8e0c094b7ccca554bc807de057e4975b
alexe1ka <leshka_09@bk.ru>
2018-01-25 17:28:43 +0300

    add new report

--- VersionExtractor/catalog_parser.py
+++ VersionExtractor/catalog_parser.py
@@ -2,37 +2,29 @@ import os
 import fnmatch
 import chardet
 import json2html
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import QObject, QThread
+import datetime
 
 
-def find(pattern, path):
-    result = []
-    for root, dirs, files in os.walk(path):
-        for name in files:
-            if fnmatch.fnmatch(name, pattern):
-                result.append(os.path.join(root, name))  # в листе сохраняется путь до файла
-                # print("pattern: " + pattern)
-                # print(name)  # выводит имя файла
-    return result
+def universal_decoder(line):
+    if chardet.detect(line)['encoding'] == 'ascii':
+        return line.decode("ascii")
+    if chardet.detect(line)['encoding'] == 'windows-1251':
+        return line.decode("windows-1251", "ignore")
+    if chardet.detect(line)['encoding'] == 'MacCyrillic':
+        return line.decode("MacCyrillic")
+    if chardet.detect(line)['encoding'] == 'utf-8':
+        return line.decode("utf-8")
+    return ""
 
 
-def generate_report(parsed_data_from_file):
-    report_file = open('report.html', 'w')
-    message = """<html>
-    <head></head>
-    <body>"""
-    for file_info in parsed_data_from_file:
-        message += file_info
-    message += """</body>
-    </html>"""
-    report_file.write(message)
-    report_file.close()
-    return 1
+def clean_string(dirty_string):
+    clean = str(dirty_string).strip()
+    clean = str(clean).replace('\n', '')
+    return clean
 
 
-# функция парсит шапку файла:
-# описание файла(description),
-# версию(version) ....
-# так же выводит список модулей в данном файле
 def file_parser(filepath):
     list_of_file_data = []  # построчный лист с содержимым файла
     # header_data = {"modules": []}
@@ -57,37 +49,69 @@ def file_parser(filepath):
 
 
 # декодирует строку исходя из определенной кодировки
-def universal_decoder(line):
-    if chardet.detect(line)['encoding'] == 'ascii':
-        return line.decode("ascii")
-    if chardet.detect(line)['encoding'] == 'windows-1251':
-        return line.decode("windows-1251", "ignore")
-    if chardet.detect(line)['encoding'] == 'MacCyrillic':
-        return line.decode("MacCyrillic")
-    if chardet.detect(line)['encoding'] == 'utf-8':
-        return line.decode("utf-8")
-    return ""
-
 
 # функция для очистки строки от \n  и лишних пробелов
-def clean_string(dirty_string):
-    clean = str(dirty_string).strip()
-    clean = str(clean).replace('\n', '')
-    return clean
+
+
+class HdlTasker(QObject):
+    # def __init__(self) -> None:
+    #     super().__init__(self)
+
+    def run(self) -> None:
+        super().run()
+
+    @pyqtSlot()
+    def find(self, pattern, path):
+        result = []
+        for root, dirs, files in os.walk(path):
+            for name in files:
+                if fnmatch.fnmatch(name, pattern):
+                    result.append(os.path.join(root, name))  # в листе сохраняется путь до файла
+                    # print("pattern: " + pattern)
+                    # print(name)  # выводит имя файла
+        return result
+
+    @pyqtSlot()
+    def generate_report(self, parsed_data_from_file):
+        now = datetime.datetime.now()
+        now = now.strftime("%Y-%m-%d_%H:%M")
+        report_filename = "report_" + now + ".html"
+        print(str(report_filename))
+        report_file = open(report_filename, 'w')
+        message = """<html>
+        <head></head>
+        <body>"""
+        for file_info in parsed_data_from_file:
+            message += file_info
+        message += """</body>
+        </html>"""
+        report_file.write(message)
+        report_file.close()
+        return 1
+
+        # функция парсит шапку файла:
+        # описание файла(description),
+        # версию(version) ....
+        # так же выводит список модулей в данном файле
 
 
 if __name__ == "__main__":
-    # test_dir_path = 'E:\!TEST_FOLDER\\v_new'
+
+    test_dir_path = 'E:\!TEST_FOLDER\\v_new'
     # test_dir_path = 'E:\!TEST_FOLDER'
-    test_dir_path = 'E:\!test'
+    # test_dir_path = 'E:\!test'
     # test_dir_path = 'D:\MyFiles\Projects\PyCharmProjects\VersionExtractor\VersionExtractor\!TEST_FOLDER\\v_new'
     # test_file_path = 'E:/addr_cntr.v'
     # test_file_path = 'D:/addr_cntr.v'
     # test_file_path = 'D:/many_modules.v'
-    file_list = find("*.v", test_dir_path)
+    tasker = HdlTasker()
+    file_list = tasker.find("*.v", test_dir_path)
+    thread = QThread()
+    tasker.moveToThread(thread)
+
     hdl_report_list = []
     for f in file_list:
         hdl_report_list.append(file_parser(f))
         print(json2html.json2html.convert(hdl_report_list))
-        generate_report(json2html.json2html.convert(hdl_report_list))
+        tasker.generate_report(json2html.json2html.convert(hdl_report_list))
         print('\n')
--- VersionExtractor/gui_start.py
+++ VersionExtractor/gui_start.py
@@ -1,14 +1,16 @@
-import VersionExtractorMainWindow
-from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtGui import QStandardItemModel
-from PyQt5.QtGui import QStandardItem
-from PyQt5.QtCore import QThread
 import os
-import catalog_parser
+
 import json2html
-import TaskThread
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtCore import QThread
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QStandardItem
+from PyQt5.QtGui import QStandardItemModel
+from PyQt5.QtWidgets import *
+
+import VersionExtractorMainWindow
+from catalog_parser import HdlTasker
+import catalog_parser
 
 
 # pyuic5 input.ui -o output.py
@@ -21,13 +23,19 @@ class ExtractorWindow(QMainWindow, QTreeView):
         self.ui = VersionExtractorMainWindow.Ui_version_extractor_by_alexe1ka()  # сгенерированный класс из Designer'a
         self.ui.setupUi(self)
 
+        # потоки для выполнения тяжелых фоновых задач
+        self.tasker = HdlTasker()
+        self.thread = QThread()
+        self.tasker.moveToThread(self.thread)
+
         self.file_path = ""  # текущий каталог
         self.files = []  # список файлов в текущем каталоге
         self.hdl_files_list = []  # список найденных hdl файлов
 
+        self.ui.progressBar.moveToThread(self.thread)
+
         model = QFileSystemModel()
         model.setFilter(QtCore.QDir.AllDirs | QtCore.QDir.NoDotAndDotDot)  # в treeView - только каталоги
-        # model.setFilter(QtCore.QDir.Files)  # фильтр на "только файлы"
         model.setRootPath(QtCore.QDir.currentPath())
         self.ui.catalogTreeView.setModel(model)
 
@@ -37,7 +45,7 @@ class ExtractorWindow(QMainWindow, QTreeView):
         self.ui.catalogTreeView.setColumnHidden(3, True)
 
         # регистрация эвентов для кнопок
-
+        # self.ui.startSearchingButton.clicked.connect(self.start_search_hdl_button_click)
         self.ui.startSearchingButton.clicked.connect(self.start_search_hdl_button_click)
         self.ui.generateReportButton.clicked.connect(self.generate_report_button_click)
 
@@ -45,13 +53,12 @@ class ExtractorWindow(QMainWindow, QTreeView):
 
     @pyqtSlot()
     def start_search_hdl_button_click(self):
-        print("start search hdl files")
         pattern = self.ui.selectFileExtension.currentText()
         # TODO паттерн для v AND vhd не работает
         # if str(pattern) == "*.v/ *.vhd":
         #     new_pattern = "*.v*d"
         #     print("pattern for all hdl files: " + new_pattern)
-        self.hdl_files_list = catalog_parser.find(pattern, self.file_path)
+        self.hdl_files_list = self.tasker.find(pattern, self.file_path)
         hdl_list_model = QStandardItemModel()
         for f in self.hdl_files_list:
             item = QStandardItem(f)
@@ -60,16 +67,19 @@ class ExtractorWindow(QMainWindow, QTreeView):
 
     @pyqtSlot()
     def generate_report_button_click(self):
+
         report_data = []
+        print("hdl_file_list: " + str(self.hdl_files_list))
         for file in self.hdl_files_list:
             report_data.append(catalog_parser.file_parser(file))
-        catalog_parser.generate_report(json2html.json2html.convert(report_data))
+        self.tasker.generate_report(json2html.json2html.convert(report_data))
         print(report_data)
 
     def click_on_dir(self, signal):
         self.file_path = self.ui.catalogTreeView.model().filePath(signal)
 
-        self.files = [f for f in os.listdir(self.file_path) if os.path.isfile(os.path.join(self.file_path, f))]  # список файлов в file_path
+        self.files = [f for f in os.listdir(self.file_path) if
+                      os.path.isfile(os.path.join(self.file_path, f))]  # список файлов в file_path
 
         list_model = QStandardItemModel()
         # TODO если диск пустой - вылетает
@@ -81,7 +91,6 @@ class ExtractorWindow(QMainWindow, QTreeView):
 
 if __name__ == "__main__":
     import sys
-    import PyQt5
 
     # pyqt_plugins = os.path.join(os.path.dirname(PyQt5.__file__), "..", "..", "..", "Library", "plugins")
     # QApplication.addLibraryPath(pyqt_plugins)
